7cd3e4da2525df6b914a74e5faaa8c91447c63cf
David Tran <frozenpho@gmail.com>
2021-12-05 10:17:50 -0700

    Move x button to the correct location

--- src/App.js
+++ src/App.js
@@ -30,7 +30,7 @@ class App extends Component {
       return movie.id === id
     })
 
-    this.setState({movie: {id: 1, title: "Fake Movie Title", poster_path: "https://image.tmdb.org/t/p/original//7G2VvG1lU8q758uOqU6z2Ds0qpA.jpg", backdrop_path: "https://image.tmdb.org/t/p/original//oazPqs1z78LcIOFslbKtJLGlueo.jpg", release_date: "2019-12-04", overview: "Some overview that is full of buzzwords to attempt to entice you to watch this movie! Explosions! Drama! True love! Robots! A cute dog!", average_rating: 6, genres: ["Drama"], budget:63000000, revenue:100853753, runtime:139, tagline: "It's a movie!" }})
+    this.setState({movie: {id: 1, title: "Fake Movie Title", poster_path: "https://image.tmdb.org/t/p/original//7G2VvG1lU8q758uOqU6z2Ds0qpA.jpg", backdrop_path: "https://image.tmdb.org/t/p/original//oazPqs1z78LcIOFslbKtJLGlueo.jpg", release_date: "2019-12-04", overview: "Some overview that is full of buzzwords to attempt to entice you to watch this movie! Explosions! Drama! True love! Robots! A cute dog!", average_rating: 6, genres: ["Drama", "Comedy"], budget:63000000, revenue:100853753, runtime:139, tagline: "It's a movie!" }})
 
     // this.setState({ movie: {title: findMovie[0].title, backdropPath: findMovie[0].backdrop_path, releaseDate: findMovie[0].release_date,
     // overview: findMovie[0].overview, averageRating: findMovie[0].average_rating, genres: findMovie[0].genres, budget: findMovie[0].budget,
