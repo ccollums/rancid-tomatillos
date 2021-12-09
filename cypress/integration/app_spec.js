90e3e383de3a372c6907aef4e68e52652bbdf07a
David Tran <frozenpho@gmail.com>
2021-12-09 14:30:57 -0700

    Update app tests

--- cypress/integration/app_spec.js
+++ cypress/integration/app_spec.js
@@ -1,17 +1,22 @@
+
+
 describe('Rancid Tomatillos', () => {
     beforeEach(() => {
-      cy.visit('http://localhost:3000');
+      cy.visit('http://localhost:3000/');
+      
     })
 
     it('should be able to visit the site and see a title and form displayed', () => {
-        cy.contains('Rancid Tomatillos')
+  
+      cy.contains('Rancid Tomatillos')
         .get('form')
         .get('input[type=text]')
       });
 
-      it.skip('should display the application logo on page load', () => {
-
-      })
+      // it.skip('should display the application logo on page load', () => {
+      // cy.get('.logo')
+      //   .should()
+      // })
 
       it('should be able to select and type in a movie into the search bar', () => {
         cy.get('input[type="text"]')
@@ -19,8 +24,16 @@ describe('Rancid Tomatillos', () => {
           .should('have.value', 'Mulan')
       })
 
-      it.skip('should show movies when the page loads', () => {
-         cy.fixture('sample-movies.json').then()
+      // it.only('should be be able to clear the search bar and see the main page again', () => {
+      //   cy.get('input[type="text]')
+      //     .type('')
+          
+      // })
+
+      it.only('should show movies when the page loads', () => {
+        cy.intercept("GET", 'https://rancid-tomatillos.herokuapp.com/api/v2/movies')
+          cy.fixture('../fixtures/sample-movies.json')
+      
       })
 
       it.skip('should start to filter movies when a movie is typed into the input bar', () => {
